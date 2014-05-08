c50d31069b513cb391802388dce8dde09f17de80
Uriah Adams <adams.uriah@gmail.com>
2014-05-08 16:09:34 -0500

    Assignment: Create the video model, and seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(
+    title: 'Futurama',
+    description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
+    small_cover_url: 'tmp/futurama.jpg'
+)
+
+Video.create(
+    title: 'Monk',
+    description: '',
+    small_cover_url: 'tmp/monk.jpg',
+    large_cover_url: 'tmp/monk_large.jpg'
+)
+
+Video.create(
+    title: 'South Park',
+    description: '',
+    small_cover_url: 'tmp/south_park.jpg'
+)
+
+Video.create(
+    title: 'Family Guy',
+    description: '',
+    small_cover_url: 'tmp/family_guy.jpg'
+)
