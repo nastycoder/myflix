c543702cb86a1220defced558d9c41a7b79ed7f0
Uriah Adams <adams.uriah@gmail.com>
2014-05-12 18:08:43 -0500

    Assignment: Test the category model and associations

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Category do
+  it 'should save' do
+    category = Category.new(name: 'comedy')
+    category.save
+
+    expect(Category.first).to eq(category)
+  end
+
+  it 'should have many videos' do
+    category = Category.create(name: 'comedy')
+    family_guy = Video.create(title: 'Family Guy', category: category)
+    south_park = Video.create(title: 'South Park', category: category)
+
+    expect(category.videos.count).to be 2
+    expect(category.videos).to eq([family_guy, south_park])
+  end
+end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,4 +7,11 @@ describe Video do
 
     expect(Video.first).to eq(video)
   end
+
+  it 'should belong to category' do
+    category = Category.create(name: 'funny')
+    video = Video.create(title: 'Funny Movie', category: category)
+
+    expect(video.category).to eq(category)
+  end
 end
