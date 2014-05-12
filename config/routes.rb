c09e9741704a79c9b24b9f354aa92bb88ccd2fb1
Uriah Adams <adams.uriah@gmail.com>
2014-05-12 16:15:11 -0500

    Assignment: Show category

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Myflix::Application.routes.draw do
   resources :videos
   get 'ui(/:action)', controller: 'ui'
 
+  controller :categories do
+    get 'genre' => :index
+  end
+
   controller :videos do
     get 'home' => :index
   end
