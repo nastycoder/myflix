8a7629f9ddf7d48533e223be473436265f5863b6
Uriah Adams <adams.uriah@gmail.com>
2014-06-02 11:09:42 -0500

    Assignment: Write integration test for resetting password

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
+require 'capybara/email/rspec'
 
 # Setup simplecov to track test coverage
 require 'simplecov'
