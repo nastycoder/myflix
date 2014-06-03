7383b95602c7dab32b5a23b2b295f1f0eb4d35eb
Uriah Adams <adams.uriah@gmail.com>
2014-06-03 09:36:00 -0500

    Assignment: Use Mailgun to send out emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,15 +18,14 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'morning-plateau-2429.herokuapp.com',
+  :authentication => :plain,
   }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {host: 'morning-plateau-2429.herokuapp.com' }
 end
